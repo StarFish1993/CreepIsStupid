18b48a32d8e43be991077d85c2fb76f940f6ab48
bingxinsun <sgag6468@gmail.com>
2021-06-25 13:49:28 +0800

    (=・ω・=)

--- main.js
+++ main.js
@@ -6,20 +6,20 @@ var r=Game.rooms['E42N54'];
 var s=r.find(FIND_SOURCES_ACTIVE);
 s=s[1].energy==0?s[0]:s[1];
 for(var i in Game.creeps) {
-    
+
     var creep=Game.creeps[i];
-    if(creep.store.getUsedCapacity()==0){
+    if(creep.store.getUsedCapacity()===0){
         creep.memory.isWork=false;
     }
-    if(creep.store.getFreeCapacity()!=0&&!creep.memory.isWork){
+    if(creep.store.getFreeCapacity()!==0&&!creep.memory.isWork){
         creep.moveTo(s);
         creep.harvest(s);
-    }else if(f.store.getFreeCapacity(RESOURCE_ENERGY)!=0){
+    }else if(f.store.getFreeCapacity(RESOURCE_ENERGY)!==0){
         creep.moveTo(f);
         creep.transfer(f,RESOURCE_ENERGY,f.store.getFreeCapacity());
-    }else if(creep.store.getUsedCapacity!=0){
+    }else if(creep.store.getUsedCapacity!==0){
         creep.memory.isWork=true;
-        if(creep.upgradeController(r.controller)==-9)
+        if(creep.upgradeController(r.controller)===-9)
             creep.moveTo(r.controller);
     }
     famerSize++;
