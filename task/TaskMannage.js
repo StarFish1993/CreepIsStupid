19210dac2d8414d43b0baabd049b3ebb1a75df85
yuheng <yuhengsun@qq.com>
2021-06-29 17:24:45 +0800

    完成了任务分配的代码

--- task/TaskMannage.js
+++ task/TaskMannage.js
@@ -9,12 +9,12 @@ export var TaskMannage = {
      * @param {Array<{type:String,id:String}>} arg 
      */
     push: function (name, id, creepAmount, roles, arg) {
-        Memory.tasks[id]={
-            name:name,
-            creepAmount:creepAmount,
-            roles:roles,
-            creeps:[],
-            arg:arg
+        Memory.tasks[id] = {
+            name: name,
+            creepAmount: creepAmount,
+            roles: roles,
+            creeps: [],
+            arg: arg
         }
     },
     /**
@@ -25,22 +25,58 @@ export var TaskMannage = {
 
     },
     /**
-     * @todo 核心部分未完成
+     * @todo 没测试
      * 统一为没有工作的creeps分配工作
      * @param {{role:Array<Creep>}} creeps
+     * @returns {{role:Array<Creep>}}
+     * 返回分工分配到工作的creep
      */
-    settleTask:function(creeps){
-        var tasks=Memory.task;
-        var ps={};
-        _.forIn(tasks,function(id,task){
-            var amount=needCreep(task);
-            if(amount>0){
-                var roles=task.roles;
-                roles.forEach(role => {
-                    var cs=creeps[role];            
-                });
+    settleTask: function (creeps) {
+        var tasks = Memory.task;
+        forInTasks(tasks, 0);
+
+        return creeps;
+
+        function forInTasks(tasks, roleIndex) {
+            var ri = roleIndex;
+            //标记是否计算了下一个优先级
+            var rif = true;
+            //为所有任务分配第 roleIndex 优先级角色的creep
+            _.forIn(tasks, (task, id) => {
+
+                var amount = needCreep(task);
+
+                var roles = task.roles;
+                //确保该任务需要人手，且该优先级下有对应角色
+                if (amount > 0 && roles.length <= roleIndex + 1) {
+                    
+                    var cs = creeps[roles[roleIndex]];
+
+                    var workCreeps = _.slice(cs, 0, amount);
+                    
+                    var cs=_.drop(cs,amount);
+                    //如果该角色没有creep了，这将其设置为undefined
+                    creeps[roles[roleIndex]]=cs.length===0?undefined:cs;
+
+                    workCreeps.forEach(creep => {
+                        creep.memory.task = id;
+                        task.creeps.push(creep.id);
+                    })
+
+                    //当该任务有下一个优先级角色，且下一优先级没有被计算过，且该任务还需要人手
+                    if (roles.length > roleIndex + 1 && rif && needCreep(task) !== 0) {
+                        ri++;
+                        rif = false;
+                    }
+
+                }
+
+            });
+            // 如果还有任务没有分配人手，且有下一个优先级的角色可用
+            if (!rif) {
+                forInTasks(tasks, ri);
             }
-        });
+        }
     },
     /**
      * 判断建筑是否存在任务
@@ -58,6 +94,6 @@ export var TaskMannage = {
      * @returns {Number}
      */
     needCreep: function (task) {
-        return task.creepAmount-task.creeps.length;
+        return task.creepAmount - task.creeps.length;
     }
 }
